1739a25f6cd4c4fbf27c94f79921fa883a3b796d
Kristina <kris2015.lebedeva@yandex.ru>
2020-07-04 00:47:17 +0300

    Tests added. 100% coveredratio - branches

--- src/main/java/ru/netology/Conditioner.java
+++ src/main/java/ru/netology/Conditioner.java
@@ -4,8 +4,37 @@ public class Conditioner {
     private String name;
     private int maxTemperature = 30;
     private int minTemperature = -15;
-    private int currentTemperature = 17;
+    private int currentTemperature;
     private boolean on;
+    private int increaseCurrentTemperature;
+    private int decreaseCurrentTemperature;
+
+
+    public int getDecreaseCurrentTemperature() {
+        return decreaseCurrentTemperature;
+    }
+    public void setDecreaseCurrentTemperature(int decreaseCurrentTemperature) {
+        if (decreaseCurrentTemperature > minTemperature) {
+            decreaseCurrentTemperature--;
+        } else {
+            return;
+        }
+        this.decreaseCurrentTemperature = decreaseCurrentTemperature;
+    }
+
+
+    public int getIncreaseCurrentTemperature() {
+        return increaseCurrentTemperature;
+    }
+    public void setIncreaseCurrentTemperature(int increaseCurrentTemperature) {
+        if (increaseCurrentTemperature < maxTemperature) {
+            increaseCurrentTemperature++;
+        } else {
+            return;
+        }
+        this.increaseCurrentTemperature = increaseCurrentTemperature;
+    }
+
 
     public int getMaxTemperature() {
         return maxTemperature;
@@ -14,12 +43,14 @@ public class Conditioner {
         this.maxTemperature = maxTemperature;
     }
 
+
     public int getMinTemperature() {
         return minTemperature;
     }
     public void setMinTemperature(int minTemperature) {
         this.minTemperature = minTemperature;
     }
+
 
     public String getName() {
         return name;
@@ -28,19 +59,14 @@ public class Conditioner {
         this.name = name;
     }
 
+
     public int getCurrentTemperature() {
         return currentTemperature;
     }
     public void setCurrentTemperature(int currentTemperature) {
-        if (currentTemperature > maxTemperature) {
-            return;
-        }
-        if (currentTemperature < minTemperature) {
-            return;
-        }
-//        проверяем, что все проверки прошли
         this.currentTemperature = currentTemperature;
     }
+
 
     public boolean isOn() {
         return on;
--- src/test/java/ru/netology/ConditionerTest.java
+++ src/test/java/ru/netology/ConditionerTest.java
@@ -6,22 +6,36 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ConditionerTest {
 
     @Test
-    void shouldGetAndSet() {
+    void shouldIncreaseCurrentTemperature() {
         Conditioner conditioner = new Conditioner();
-        String expectedName = "Павлик Морозов";
-        assertNull(conditioner.getName());
-        conditioner.setName(expectedName);
-        assertEquals(expectedName, conditioner.getName());
+        assertEquals(0, conditioner.getIncreaseCurrentTemperature());
+        conditioner.setIncreaseCurrentTemperature(18);
+        assertEquals(19, conditioner.getIncreaseCurrentTemperature());
     }
 
     @Test
-    void shouldChangeCurrentTemperature() {
+    void shouldIncreaseCurrentMaxTemperature() {
         Conditioner conditioner = new Conditioner();
-        assertEquals(17, conditioner.getCurrentTemperature());
-        conditioner.setCurrentTemperature(20);
-        assertEquals(20, conditioner.getCurrentTemperature());
+        assertEquals(0, conditioner.getIncreaseCurrentTemperature());
+        conditioner.setIncreaseCurrentTemperature(30);
+        assertEquals(0, conditioner.getIncreaseCurrentTemperature());
     }
 
+    @Test
+    void shouldDecreaseCurrentTemperature() {
+        Conditioner conditioner = new Conditioner();
+        assertEquals(0, conditioner.getDecreaseCurrentTemperature());
+        conditioner.setDecreaseCurrentTemperature(18);
+        assertEquals(17, conditioner.getDecreaseCurrentTemperature());
+    }
+
+    @Test
+    void shouldDecreaseMinCurrentTemperature() {
+        Conditioner conditioner = new Conditioner();
+        assertEquals(0, conditioner.getDecreaseCurrentTemperature());
+        conditioner.setDecreaseCurrentTemperature(-15);
+        assertEquals(0, conditioner.getDecreaseCurrentTemperature());
+    }
 
 }
 
