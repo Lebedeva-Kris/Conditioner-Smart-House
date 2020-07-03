d12621c2b6edfc9cd00bb0e3d2826cf1b64a087f
Kristina <kris2015.lebedeva@yandex.ru>
2020-07-03 22:55:09 +0300

    Build failure

--- src/test/java/ru/netology/ConditionerAdvancerTest.java
+++ src/test/java/ru/netology/ConditionerAdvancerTest.java
@@ -1,31 +1,32 @@
-//package ru.netology;
-//
-//import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//public class ConditionerAdvancerTest {
-//
-//    @Test
-//    void createConditioner() {
-//        ConditionerAdvancer conditioner = new ConditionerAdvancer();
-//    }
-//
-//    @Test
-//    void shouldGetAndSet() {
-//        ConditionerAdvancer conditioner = new ConditionerAdvancer();
-//        String expectedName = "Павлик Морозов";
-//        assertNull(conditioner.getName());
-//        conditioner.setName(expectedName);
-//        assertEquals(expectedName, conditioner.getName());
-//    }
-//
-//    @Test
-//    void shouldChangeCurrentTemperature() {
-//        ConditionerAdvancer conditioner = new ConditionerAdvancer();
-//        assertEquals(0, conditioner.getCurrentTemperature());
-//        conditioner.setCurrentTemperature(20);
-//        assertEquals(20, conditioner.getCurrentTemperature());
-//    }
-//
-//
-//}
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ConditionerAdvancerTest {
+
+    @Test
+    void createConditioner() {
+        ConditionerAdvancer conditioner = new ConditionerAdvancer();
+    }
+
+    @Test
+    void shouldGetAndSet() {
+        ConditionerAdvancer conditioner = new ConditionerAdvancer();
+        String expectedName = "Павлик Морозов";
+        assertNull(conditioner.getName());
+        conditioner.setName(expectedName);
+        assertEquals(expectedName, conditioner.getName());
+    }
+
+    @Test
+    void shouldChangeCurrentTemperature() {
+        ConditionerAdvancer conditioner = new ConditionerAdvancer();
+        assertEquals(0, conditioner.getCurrentTemperature());
+        conditioner.setCurrentTemperature(20);
+        assertEquals(20, conditioner.getCurrentTemperature());
+    }
+
+
+}
+
